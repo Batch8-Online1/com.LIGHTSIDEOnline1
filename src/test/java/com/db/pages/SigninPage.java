4eb7094726fc09621a10b68d63e3c7b6d54b021c
AYLIN <aylin@MacBook-Pro-de-AYLIN.local>
2018-08-22 21:56:17 -0700

    added structure for backendtesting

---
+++ src/test/java/com/db/pages/SigninPage.java
@@ -0,0 +1,23 @@
+package com.db.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.LIGHTSIDEOnline1.utilities.Driver;
+
+public class SigninPage {
+	public SigninPage() {
+		PageFactory.initElements(Driver.getDriver(), this);
+	}
+
+	@FindBy(name = "email")
+	public WebElement email;
+
+	@FindBy(name = "password")
+	public WebElement password;
+
+	@FindBy(xpath = "//button[.='sign in']")
+	public WebElement signInButton;
+
+}
